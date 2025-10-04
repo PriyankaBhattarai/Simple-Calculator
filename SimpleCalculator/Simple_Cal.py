872f17ae67df61482d4ecf40bd07c4d0fc12bb4b
PriyankaBhattarai <priyankabhattarai134@gmail.com>
2025-10-04 16:13:09 +0530

    added modulus operator

--- SimpleCalculator/Simple_Cal.py
+++ SimpleCalculator/Simple_Cal.py
@@ -1,8 +1,8 @@
 # Simple Calculator
 
-N1 = int(input("Enter first number: ")) # Input first number as integer.
-N2 = int(input("Enter second number: ")) # Input second number as integer.
-op = input("Enter operator (+, -, *, /): ") # Input op is (operator).
+N1 = float(input("Enter first number: ")) # Input first number as float.
+N2 = float(input("Enter second number: ")) # Input second number as float.
+op = input("Enter operator (+, -, *, /, %): ") # Input op is (operator).
 if op == '+':
     print(N1 + N2) # Perform addition and print the result.
 elif op == '-':
@@ -14,6 +14,11 @@ elif op == '/':
         print(N1 / N2) # Perform division and print the result.
     else:
         print("Error: Division by zero") # Handle division by zero error.
+elif op == '%':
+    if N2 != 0:
+        print(N1 % N2) # Perform modulus and print the result.
+    else:
+        print("Error: Modulus by zero") # Handle division by zero error.
 else:
     print("Invalid operator")   
 
